663cac6050279d6243b7a2b43c8109421e232dda
Khaled Mohammad <khaledyasser2002@gmail.com>
2023-01-05 20:19:59 +0900

    kind of broken will fix tmrw

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db'
+db=SQLAlchemy(app)
+
+class Todo(db.Model):
+    email = db.Column(db.String, primary_key=True)
+    passwrod = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return "<Task %r>" % self.id
 
 @app.route("/")
 def index():
