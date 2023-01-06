1a644ab2abe6239167dafb36044ee8ad62cdd24e
Khaled Mohammad <khaledyasser2002@gmail.com>
2023-01-06 18:27:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,40 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///accountTracker.db'
-
 db=SQLAlchemy(app)
-#hi
+
 class Login_Manager(db.Model):
-    name = db.Column(db.String, primary_key=True)
+    email = db.Column(db.String, primary_key=True)
     password = db.Column(db.String, nullable=False)
-    email=db.Column(db.String, nullable=False)
+    name=db.Column(db.String, nullable=False)
 
     def __repr__(self):
         return "<Task %r>" % self.id
 
-# with app.app_context():
-#    db.create_all()
+with app.app_context():
+   db.create_all()
 
-@app.route("/")
-def index():
+@app.route("/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        return redirect("/book")
     return render_template("Login.html")
 
-@app.route("/register")
+@app.route("/register", methods=["POST", "GET"])
 def register():
     if request.method == "POST":
-        # email = request.form["email"]
-        # name = request.form["name"]
-        # password = request.form["password"]
-        # lgin = Login_Manager(email=email, password=password, name=name)
-        # db.session.add(lgin)
-        # db.session.commit()
         return redirect("/book")
     return render_template("Register.html")
 
-@app.route("/book")
+@app.route("/book", methods=["POST", "GET"])
 def book():
+    if request.method == "POST":
+        return redirect("/pay")
     return render_template("Booking.html")
 
-@app.route("/pay")
+@app.route("/pay", methods=["POST", "GET"])
 def pay():
     return render_template("Payment.html")
 
