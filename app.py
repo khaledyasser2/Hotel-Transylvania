5ce2a07e8957ded2e32c11d7a9e2c1a4039ec0f4
Khaled Mohammad <khaledyasser2002@gmail.com>
2023-01-06 16:48:13 +0900

    Register, login page now take to each other

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, Responsepip
+from flask import Flask, render_template, url_for, Response
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
